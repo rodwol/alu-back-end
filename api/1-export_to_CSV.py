989e34771fe058f10689d395481282bd9885886c
rodwol <r.goniche@alustudent.com>
2024-06-29 12:43:25 +0100

    commit

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,7 +1,36 @@
 #!/usr/bin/python3
 """
-Export data from an API to CSV format.
+    python script that exports data in the CSV format
 """
+import csv
+import json
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    request_employee = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
+    user = json.loads(request_employee.text)
+
+    username = user.get("username")
+
+    request_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1]))
+    tasks = {}
+
+    user_todos = json.loads(request_todos.text)
+
+    for dictionary in user_todos:
+        tasks.update({dictionary.get("title"): dictionary.get("completed")})
+
+    with open('{}.csv'.format(argv[1]), mode='w') as file:
+        file_editor = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
+        for k, v in tasks.items():
+            file_editor.writerow([argv[1], username, v, k])
+
+"""
+#!/usr/bin/python3
 import csv
 import requests
 import sys
@@ -28,3 +57,4 @@ if __name__ == "__main__":
         for todo in response2:
             writer.writerow([EMPLOYEE_ID, EMPLOYEE_NAME, todo.get('completed'), todo.get('title')])
             # writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+"""
